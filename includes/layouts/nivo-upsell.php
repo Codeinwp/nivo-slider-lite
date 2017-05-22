2470ba4fcb8e1412da3f4dc678f91af4767aed3c
Bogdan Preda <bogdan.preda@themeisle.com>
2017-05-22 10:34:43 +0300

    PHPCS

--- includes/layouts/nivo-upsell.php
+++ includes/layouts/nivo-upsell.php
@@ -1,33 +1,35 @@
-<!--
-Layout For Upsell Page of Nivo Slider
-
-@since    3.0.12
-@package  feedzy-rss-feeds
--->
+<?php
+/**
+ * Layout For Upsell Page of Nivo Slider
+ *
+ * @since    3.0.12
+ * @package  feedzy-rss-feeds
+ */
+?>
 <div id="pro-features">
 
-    <div class="pro-features-header">
-        <p class="logo">Nivo Slider</p>
-        <span class="slogan">by <a
-                href="https://themeisle.com/">ThemeIsle</a></span>
-        <div class="header-btns">
+	<div class="pro-features-header">
+		<p class="logo">Nivo Slider</p>
+		<span class="slogan">by <a
+				href="https://themeisle.com/">ThemeIsle</a></span>
+		<div class="header-btns">
 
-            <a target="_blank" href="https://themeisle.com/plugins/nivo-slider/" class="buy-now"><span class="dashicons dashicons-cart"></span> Buy now</a>
-        </div>
-        <div class="clear"></div>
-    </div>
+			<a target="_blank" href="https://themeisle.com/plugins/nivo-slider/" class="buy-now"><span class="dashicons dashicons-cart"></span> Buy now</a>
+		</div>
+		<div class="clear"></div>
+	</div>
 
-    <div class="pro-feature">
-        <div class="pro-feature-features">
-            <h2>Placeholder for PRO features title</h2>
-            <h4>Feature One</h4>
-            <p>Lorem ipsum dolores ...</p>
-            <h4>Feature Two</h4>
-            <p>Lorem ipsum dolores ...</p>
-        </div>
-        <div class="pro-feature-image">
-        </div>
-        <div class="clear"></div>
-    </div>
-    <div class="clear"></div>
+	<div class="pro-feature">
+		<div class="pro-feature-features">
+			<h2>Placeholder for PRO features title</h2>
+			<h4>Feature One</h4>
+			<p>Lorem ipsum dolores ...</p>
+			<h4>Feature Two</h4>
+			<p>Lorem ipsum dolores ...</p>
+		</div>
+		<div class="pro-feature-image">
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="clear"></div>
 </div>
